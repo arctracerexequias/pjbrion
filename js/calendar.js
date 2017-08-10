0b89e397d578f4e72fe2e3a1bdcebd3ad9c45f5d
Peter Joshua Brion <exequias1022@gmail.com>
2017-08-10 21:34:22 +0800

    added something out there

--- js/calendar.js
+++ js/calendar.js
@@ -1,9 +1,17 @@
-$(document).ready(function() {
-
-    // page is now ready, initialize the calendar...
-
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
-
+$('#calendar').clndr({
+  template: $('#calendar-template').html(),
+  events: [
+    { date: '2013-09-09', title: 'CLNDR GitHub Page Finished', url: 'http://github.com/kylestetz/CLNDR' }
+  ],
+  clickEvents: {
+    click: function(target) {
+      console.log(target);
+    },
+    onMonthChange: function(month) {
+      console.log('you just went to ' + month.format('MMMM, YYYY'));
+    }
+  },
+  doneRendering: function() {
+    console.log('this would be a fine place to attach custom event handlers.');
+  }
 });
