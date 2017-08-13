41dd6f23d8803c7591e7d8bd636f5cee7076de3e
arctracerexequias <exequias1022@gmail.com>
2017-08-13 11:42:35 +0800

    Something happens

--- js/parallax.js
+++ js/parallax.js
@@ -1,6 +1,5 @@
-$(document).ready(function(){
-     $('.parallax').parallax();
-    });
-
-
-
+$(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    $('.parallax-header-bg').css('top', -(scrollTop * 0.4)+"px");
+    $('.parallax-header-bg2').css('top', -(scrollTop * 0.4)+"px");
+});
